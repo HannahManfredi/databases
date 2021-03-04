6f3917fa5e726fa65913bd0eec39f3f6388c53a8
Hannah Manfredi <hmanfredi@gmail.com>
2021-03-03 19:28:29 -0800

    "Complete customizing tests"

--- server/schema.sql
+++ server/schema.sql
@@ -12,20 +12,12 @@ CREATE TABLE rooms (
 
 CREATE TABLE messages (
   id int primary key,
-  messages varchar(100),
+  text varchar(100),
   room int not null,
   person int not null,
   foreign key (room) references users(id),
   foreign key (person) references rooms(id)
 );
-
--- ALTER TABLE messages
--- ADD
-
--- ALTER TABLE messages
--- ADD FOREIGN KEY (user) REFERENCES users(id);
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
